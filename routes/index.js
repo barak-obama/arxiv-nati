194156744f3a3394cf8730415f4ad4a9129e5073
Barak Obama <barak.oh16@gmail.com>
2019-03-30 15:29:53 +0300

    |dzfsa

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/arxiv.org/index.html');
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
